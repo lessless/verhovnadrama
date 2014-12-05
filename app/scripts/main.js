5785e6ab600429b4d42aabb63529f5e001ab8bcc
lessless <me@lessless.pp.ua>
2014-12-05 16:38:44 +0200

    fixed jshint errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,15 @@
-/* jshint devel:true */
-// countdown.setLabels(
-//             'миллисекунда|секунда|минута|час|день|неделя|месяц|год|декада|century|millennium',
-//             'миллисекунд|секунд|минут|часов|дней|недель|месев|года|декад|century|millennium');
-
 (function() {
-  function byId(id) {
-    return document.getElementById(id);
-  }
+  'use strict';
 
   function formatTens(n) {
     // format integers to have at least two digits
     return (n < 10) ? '0'+n : ''+n;
   }
 
-
   function update() {
-    var units = countdown.YEARS |
-                countdown.MONTHS |
-                countdown.DAYS;
+    var units = countdown.YEARS | countdown.MONTHS | countdown.DAYS; // jshint ignore:line
     var elapsed = countdown( new Date(2014, 12-1, 4), null, units );
-    console.log(elapsed.months)
+
     document.getElementById('months').innerHTML = formatTens(elapsed.months);
     document.getElementById('days').innerHTML = formatTens(elapsed.days);
     // requestAnimationFrame(update);
